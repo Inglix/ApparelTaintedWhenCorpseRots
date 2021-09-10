21ef9a40cd8e8c78fc196afb64200a61bd5c0ccd
Inglix <thraxus@gmail.com>
2021-09-10 16:13:51 -0300

    Add project files.

---
+++ Source/Apparel Tainted When Corpse Rots/ApparelTaintedOnCorpseRot.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace ApparelTaintedOnCorpseRot
+{
+    [StaticConstructorOnStartup]
+    public static class ApparelTaintedOnCorpseRot
+    {
+        static ApparelTaintedOnCorpseRot()
+        {
+            var harmony = new Harmony("Inglix.ApparelTaintedOnCorpseRot");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(Pawn_ApparelTracker))]
+    [HarmonyPatch(nameof(Pawn_ApparelTracker.Notify_PawnKilled))]
+    class PatchPawnKilled
+    {
+        static bool Prefix(Pawn_ApparelTracker __instance, DamageInfo? dinfo)
+        {
+            if (dinfo != null && dinfo.Value.Def.ExternalViolenceFor(__instance.pawn))
+            {
+                for (int i = 0; i < __instance.GetDirectlyHeldThings().Count; i++)
+                {
+                    if (__instance.GetDirectlyHeldThings()[i].def.useHitPoints)
+                    {
+                        int num = Mathf.RoundToInt((float)__instance.GetDirectlyHeldThings()[i].HitPoints * Rand.Range(0.15f, 0.4f));
+                        __instance.GetDirectlyHeldThings()[i].TakeDamage(new DamageInfo(dinfo.Value.Def, (float)num, 0f, -1f, null, null, null, DamageInfo.SourceCategory.ThingOrUnknown, null, true, true));
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Corpse))]
+    [HarmonyPatch(nameof(Corpse.RotStageChanged))]
+    class PatchRotStateChanged
+    {
+        static void Postfix(Corpse __instance)
+        {
+            Pawn corpseOwner = __instance.InnerPawn;
+            if (corpseOwner.apparel != null) 
+            {
+                ThingOwner<Apparel> corpseApparel = (ThingOwner<Apparel>)corpseOwner.apparel.GetDirectlyHeldThings();
+                for (int i = 0; i < corpseApparel.Count; i++)
+                {
+                    corpseApparel[i].Notify_PawnKilled();
+                }
+            }
+
+        }
+    }
+}
